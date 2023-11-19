de3c0e2319990f2f2ca61ab1cda729adbc7b6f46
allenlin <allenlin@nine-yi.com>
2023-11-19 08:30:16 +0800

    Fix redundant Compare

--- Generic/Exam1/MyCompareTo.cs
+++ Generic/Exam1/MyCompareTo.cs
@@ -1,59 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Exam1
 {
     public class MyCompareTo<T>
     {
+        /// Compares the specified reference value.
+        /// </summary>
+        /// <param name="refValue">The reference value.</param>
+        /// <param name="diffValue">The difference value.</param>
+        /// <returns></returns>
+        public bool Compare(int refValue, int diffValue)
+        {
+            if (refValue.GetType().IsValueType)
+            {
+                return (refValue == diffValue);
+            }
+
+            return object.Equals(refValue, diffValue);
+        }
+
         /// <summary>
-        ///// Compares the specified reference value.
-        ///// </summary>
-        ///// <param name="refValue">The reference value.</param>
-        ///// <param name="diffValue">The difference value.</param>
-        ///// <returns></returns>
-        //public bool Compare(int refValue, int diffValue)
-        //{
-        //    if (refValue.GetType().IsValueType)
-        //    {
-        //        return (refValue == diffValue);
-        //    }
+        /// Compares the specified reference value.
+        /// </summary>
+        /// <param name="refValue">The reference value.</param>
+        /// <param name="diffValue">The difference value.</param>
+        /// <returns></returns>
+        public bool Compare(double refValue, double diffValue)
+        {
+            if (refValue.GetType().IsValueType)
+            {
+                return (refValue == diffValue);
+            }
 
-        //    return object.Equals(refValue, diffValue);
-        //}
+            return object.Equals(refValue, diffValue);
+        }
 
-        ///// <summary>
-        ///// Compares the specified reference value.
-        ///// </summary>
-        ///// <param name="refValue">The reference value.</param>
-        ///// <param name="diffValue">The difference value.</param>
-        ///// <returns></returns>
-        //public bool Compare(double refValue, double diffValue)
-        //{
-        //    if (refValue.GetType().IsValueType)
-        //    {
-        //        return (refValue == diffValue);
-        //    }
+        /// <summary>
+        /// Compares the specified reference object.
+        /// </summary>
+        /// <param name="refObj">The reference object.</param>
+        /// <param name="diffObj">The difference object.</param>
+        /// <returns></returns>
+        public bool Compare(string refObj, string diffObj)
+        {
+            if (refObj.GetType().IsValueType)
+            {
+                return (refObj == diffObj);
+            }
 
-        //    return object.Equals(refValue, diffValue);
-        //}
-
-        ///// <summary>
-        ///// Compares the specified reference object.
-        ///// </summary>
-        ///// <param name="refObj">The reference object.</param>
-        ///// <param name="diffObj">The difference object.</param>
-        ///// <returns></returns>
-        //public bool Compare(string refObj, string diffObj)
-        //{
-        //    if (refObj.GetType().IsValueType)
-        //    {
-        //        return (refObj == diffObj);
-        //    }
-
-        //    return object.Equals(refObj, diffObj);
-        //}
-
+            return object.Equals(refObj, diffObj);
+        }
 
         /// <summary>
         /// 試著用一個泛型方法取代上述的三個 Compare 方法
@@ -62,15 +58,10 @@ namespace Exam1
         /// <param name="refObj">The reference object.</param>
         /// <param name="diffObj">The difference object.</param>
         /// <returns>result</returns>
-        
+
         public bool Compare(T refObj, T diffObj)
         {
-            if (refObj.GetType().IsValueType)
-            {
-                return EqualityComparer<T>.Default.Equals(refObj, diffObj);
-            }
-
-            return object.Equals(refObj, diffObj);
+            return EqualityComparer<T>.Default.Equals(refObj, diffObj);
         }
     }
 }
